2363feec598a661995cbdd1a4fd08ea8aa94e375
Birdheim <herman.bergheim@gmail.com>
2025-11-14 19:05:38 +0100

    feat: build react app using Vite

---
+++ client/src/App.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState<string>('Loafing...')
+  const [count, setCount] = useState(0)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    // Call backend health check
+    fetch('http://localhost:8000/')
+      .then(res => {
+        if (!res.ok) throw new Error('Backend not responding')
+        return res.json()
+      })
+      .then(data => {
+        setMessage(data.message)
+      })
+      .catch(err => {
+        setError(err.message)
+      })
+  }, [])
+
+  return (
+    <>
+      <div className='App'> 
+        <h1>
+          ML Playground
+        </h1>
+
+        {error ? (
+          <p style={{color: 'red'}}>Error: {error}</p>
+        ): (
+          <>
+            <p style={{color: 'green'}}>Backend message: {message}</p>
+            <p>Backend works</p>
+          </>
+        )}
+
+      </div>
+      {/* <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  )
+}
+
+export default App
